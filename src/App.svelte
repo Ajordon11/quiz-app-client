2e256b1567c902ec8424437ad3c0ff64a74612e8
Ajordon11 <andrej.fula@gmail.com>
2024-12-01 09:08:31 +0100

    Change to svetle

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { io } from "socket.io-client";
+
+  let connected: string = "Nope!";
+
+  const socket = io("https://quiz-app-server-xatw.onrender.com");
+
+  socket.on("connect", () => {
+    connected = "We got a signal!";
+  });
+
+  socket.on("server-info", (data) => {
+    console.log(data);
+    socket.emit("message", "Hello");
+  });
+</script>
+
+<main>
+  <h1>{connected}</h1>
+</main>
+
+<style>
+</style>
