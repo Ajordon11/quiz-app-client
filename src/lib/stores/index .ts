f5386927b8b8f706ef9a0fd8dab857d2208598f4
Ajordon11 <andrej.fula@gmail.com>
2024-12-28 19:04:42 +0100

    Add more songs

--- src/lib/stores/index .ts
+++ src/lib/stores/index .ts
@@ -20,7 +20,15 @@ export const countdownTime = writable<number>(5)
 export const availableSongs = writable<SelectOptionType<string>[]>([
   { value: 'Da Ba Dee', name: 'Da Ba Dee' },
   { value: 'Rebel', name: 'Rebel' },
-  { value: 'Rampa', name: 'Rampa' }
+  { value: 'Rampa', name: 'Rampa' },
+  { value: 'Dragostea Din Tei', name: 'Dragostea Din Tei' },
+  { value: 'Gimme!', name: 'Gimme!' },
+  { value: 'Song Groove', name: 'Song Groove' },
+  { value: 'The 7th Element', name: 'The 7th Element' },
+  { value: 'The Best', name: 'The Best' },
+  { value: 'Red Sun In The Sky', name: 'Red Sun In The Sky' },
+  { value: 'Rasputin', name: 'Rasputin' },
+  { value: 'Aj tak sme stale frajeri', name: 'Aj tak sme stale frajeri' }
 ])
 
 // Generate the alphabet dynamically
